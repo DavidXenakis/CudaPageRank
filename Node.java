d5200f16231de264115ac7e45cf228cf93e99ecb
Matthew Austin <matt.austin94@gmail.com>
2015-10-14 23:12:16 -0700

    Began working on it

---
+++ Node.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+
+public class Node {
+	List<Node> inlinks;
+	List<Node> outlinks;
+	double pageRank;
+}
--- PageRank.java
+++ PageRank.java
@@ -1,5 +1,33 @@
-
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PageRank {
-
+	private static final double d = .6;
+	private ArrayList<Node> graph  = null;
+	private HashMap<Node, Double> pageRank = null;
+	private int size;
+	public PageRank(ArrayList<Node> graph) {
+		this.graph = graph;
+		pageRank = new HashMap<Node, Double>();
+	}
+	public void getPageRanks() {
+		for(int i = 0; i < graph.size(); i++) {
+			Node node = graph.get(i);
+			if(pageRank.get(node) != null) {
+				System.out.println("Converged");
+				return;
+			}
+			pageRank.put(node, pageRank(node));
+		}
+	}
+	private double pageRank(Node node) {
+		double sum = 0.0;
+		if(node.outlinks.size() == 0) {
+			return 1 / size;
+		}
+		for(Node inlink : node.inlinks) {
+			sum += (1 / inlink.outlinks.size()) * pageRank(inlink);
+		}
+		return (1 - d) * (1 / size) + d * sum;
+	}
 }
