4d1f5a8280132fdfff70065b15ebf3ddb49ac2ba
Max Bendick <maxbendick@gmail.com>
2015-11-11 11:57:07 -0800

    PageRankSparse more fleshed out

--- c++/PageRankSparse.cpp
+++ c++/PageRankSparse.cpp
@@ -9,54 +9,40 @@ v_quadratic_error: quadratic error allowed
 
 returns a dense vector of ranks
 */
+#define QUADRATIC_ERROR .001
+#define DAMPING_FACTOR .85
 
-float *pagerank(M, d, v_quadratic_error) {
+float *pagerank(SparseMatrix M) {
    int n = M.numRows();
-   float *v = 
-      /*float * of size n, filled with random numbers*/
-      malloc(sizeof(float) * n);
-   v =/* v ./ norm(v, 1) =
-         the vector from dividing elements of v / norm(v, 1),
-         where norm(v, 1) = sum(abs(v)) = sum of element magnitudes
-      */
-      v;
+   float *v = malloc(sizeof(float) * n);
+   std::fill(v, v+n, 1/n);
 
-   float *last_v = 
-      /*
-        ones(N, 1) * inf =
-        length n vector of infinite values
-      */
-      malloc(sizeof(float) * n);
+   float *last_v = malloc(sizeof(float) * n);
 
+   M_hat = sparse_MXplusB(M, DAMPING_FACTOR, (1-DAMPING_FACTOR)/n);
 
-   M_hat = 
-      /*
-        (d .* M) + (((1 - d) / N) .* ones(N, N))
-                where d .* M = multiply each element of M by d
-        and ones(N, N) = an N by N array of 1s
-        
-        M_hat = d * colNormalize(M) + k
+   float error;
+   do {
+      last_v = v;
+      v = matrixVectorMultiply(M_hat, v);
 
-      */
-      malloc(sizeof(float *) * n);
+      error = vectorSubtractAndNormalize2(v, last_v, n);
+      //    = sum( (v[i] - last_v[i])^2 ) ^ .5 = norm(v - last_v, 2)
 
-   int k = (1 - d) / n;
-   // add matrix d .* M to n by n matrix of all k's
-   // == add k to every element of d .* M 
-
-   // d * M + k = (.85 * M) + .03 
+   } while (error > QUADRATIC_ERROR);
 
 
-   /*
-   while(norm(v - last_v, 2) > v_quadratic_error)
-   where norm(v - last_v, 2) = sum( (diff[i])^2 ) ^ (1/2)
-   where diff[i] = v[i] - last_v[i]
-   */
-   while(error > v_quadratic_error) {
-      /*last_v = v;
-      v = M_hat * v;
-         % removed the L2 norm of the iterated PR
-         end*/
-   }
-
+   free(v);
+   free(M_hat);
+   free(last_v);
 }
+
+/* TODO
+
+float *sparse_MatrixVectorMultiply(SparseMatrix M, float *v, int n)
+
+SparseMatrix *sparse_MXplusB(SparseMatrix X, float m, float b, int n)
+
+float vectorSubtractAndNormalize2(float *v, float *last_v, int n)
+
+*/
